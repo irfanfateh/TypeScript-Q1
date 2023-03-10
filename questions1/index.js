dfb118de98d849ee6a9a92159f1e917345cef761
irfanfateh <irfanfateh0@gmail.com>
2023-03-10 21:12:07 +0500

    Quarter1 Questions

---
+++ questions1/index.js
@@ -0,0 +1,7 @@
+// let age:number=24;
+// let myString:string='Muhammad irfan';
+// console.log(`My name is : ${myString} and my age is: ${age}`);
+var x = 23;
+{
+    console.log(x);
+}
---
+++ questions1/index.ts
@@ -0,0 +1,9 @@
+// let age:number=24;
+// let myString:string='Muhammad irfan';
+// console.log(`My name is : ${myString} and my age is: ${age}`);
+
+let x=23;
+
+{
+    console.log(x);
+}
