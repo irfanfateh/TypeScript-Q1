8d32dbc4ad76df43478a5e1d58c05b441104e973
irfanfateh <irfanfateh0@gmail.com>
2023-04-06 11:45:44 +0500

    correction

--- question3/index.js
+++ question3/index.js
@@ -1,2 +1,9 @@
-var personName = "Muhammad";
-console.log("Hello ".concat(personName, ", would you like to learn some Python today?"));
+var personName = "muhammad irfan";
+console.log("Lowercase: " + personName.toLowerCase());
+console.log("Uppercase: " + personName.toUpperCase());
+// Print person's name in titlecase
+var words = personName.split(" ");
+var titlecase = words.map(function (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}).join(" ");
+console.log("Titlecase: " + titlecase);
--- question3/index.ts
+++ question3/index.ts
@@ -1,2 +1,11 @@
-let personName="Muhammad";
-console.log(`Hello ${personName}, would you like to learn some Python today?`);
+const personName: string = "muhammad irfan";
+
+console.log("Lowercase: " + personName.toLowerCase());
+console.log("Uppercase: " + personName.toUpperCase());
+
+// Print person's name in titlecase
+const words = personName.split(" ");
+const titlecase = words.map(word => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}).join(" ");
+console.log("Titlecase: " + titlecase);
